b8e4afa3f94216849837b5353f159c7771ca1b14
MichaelHazielAmbrocioPerez <71995114+MichaelHazielAmbrocioPerez@users.noreply.github.com>
2020-12-01 18:04:46 -0600

    Create NodoPila.java

---
+++ U3E4/NodoPila.java
@@ -0,0 +1,11 @@
+package u3e4;
+public class NodoPila {
+
+    int dato;
+    NodoPila siguiente;
+
+    public NodoPila(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }    
+}
